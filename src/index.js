2feae1daf7a94c4ea00b623ea9d35ee687156280
Zefevr <zefevillalba@gmail.com>
2018-07-17 13:49:54 +0200

    refactoring searchbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyD8k1GaWpECPdAfJa9A9-OydmIXVPXIKk4'
 // Create a new component.
 // This component should produce some HTML
 
 const App = () => {
-    return <div>Hi Cefe!</div>
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    )
 }
 
 // take this component's HTML
